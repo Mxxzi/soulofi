aba6dde2b93446512ea93953e6c5d918744c57f0
Mxxzi <mohsinak570@gmail.com>
2023-06-09 23:08:54 +0530

    removed unwanted code

--- lib/splash.dart
+++ lib/splash.dart
@@ -36,7 +36,6 @@ class _SplashScreenState extends State<SplashScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
